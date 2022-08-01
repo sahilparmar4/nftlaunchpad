1cef407e52923540b0b228ced2febfded3aea01f
Sahil Parmar <78207908+sahilparmar4@users.noreply.github.com>
2022-08-01 14:56:45 +0530

    Create  live_auction.js

---
+++ website/components/nft_details/live_auction.js
@@ -0,0 +1,96 @@
+const LiveAuction = () => {
+    return (
+        <>
+            <section className="top-collection-area live-auctions-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="section-title mb-40">
+                                <h2 className="title">Live Auctions <picture><img src="assets/img/icons/title_icon01.png" alt="" /></picture></h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row top-collection-active">
+                        <div className="col-xl-3">
+                            <div className="top-collection-item">
+                                <div className="collection-item-thumb">
+                                    <div className="shield-icon"><picture>
+                                        <img alt="" src="assets/img/others/shield.png" /></picture>
+                                    </div>
+                                    <a href="#"><picture><img src="assets/img/others/1top_collection01.jpg" alt="" /></picture></a>
+                                </div>
+                                <div className="collection-item-content">
+                                    <h5 className="title"><a href="#">NFT Collection</a> <span className="price">5.4 ETH</span></h5>
+                                </div>
+                                <div className="collection-item-bottom">
+                                    <ul>
+                                        <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                                        <li className="wishlist"><a href="#">59</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-3">
+                            <div className="top-collection-item">
+                                <div className="collection-item-thumb">
+                                    <div className="shield-icon"><picture>
+                                        <img alt="" src="assets/img/others/shield.png" /></picture>
+                                    </div>
+                                    <a href="#"><picture><img src="assets/img/others/2top_collection01.jpg" alt="" /></picture></a>
+                                </div>
+                                <div className="collection-item-content">
+                                    <h5 className="title"><a href="#">NFT Collection</a> <span className="price">5.4 ETH</span></h5>
+                                </div>
+                                <div className="collection-item-bottom">
+                                    <ul>
+                                        <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                                        <li className="wishlist"><a href="#">59</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-3">
+                            <div className="top-collection-item">
+                                <div className="collection-item-thumb">
+                                    <div className="shield-icon"><picture>
+                                        <img alt="" src="assets/img/others/shield.png" /></picture>
+                                    </div>
+                                    <a href="#"><picture><img src="assets/img/others/3top_collection01.jpg" alt="" /></picture></a>
+                                </div>
+                                <div className="collection-item-content">
+                                    <h5 className="title"><a href="#">NFT Collection</a> <span className="price">5.4 ETH</span></h5>
+                                </div>
+                                <div className="collection-item-bottom">
+                                    <ul>
+                                        <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                                        <li className="wishlist"><a href="#">59</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-3">
+                            <div className="top-collection-item">
+                                <div className="collection-item-thumb">
+                                    <div className="shield-icon"><picture>
+                                        <img alt="" src="assets/img/others/shield.png" /></picture>
+                                    </div>
+                                    <a href="#"><picture><img src="assets/img/others/4top_collection01.jpg" alt="" /></picture></a>
+                                </div>
+                                <div className="collection-item-content">
+                                    <h5 className="title"><a href="#">NFT Collection</a> <span className="price">5.4 ETH</span></h5>
+                                </div>
+                                <div className="collection-item-bottom">
+                                    <ul>
+                                        <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                                        <li className="wishlist"><a href="#">59</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+export default LiveAuction;
