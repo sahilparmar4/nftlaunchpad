3d67769b5fcd70aa09b2af1a2f867b63b20df859
Sahil Parmar <78207908+sahilparmar4@users.noreply.github.com>
2022-08-01 14:53:55 +0530

    Create fraction_step_1_main.js

---
+++ website/components/fractionalize/fraction_step_1_main.js
@@ -0,0 +1,78 @@
+
+import Link from "next/link";
+
+const FractionStep1Main = () => {
+
+    return (<>
+        <section className="breadcrumb-area breadcrumb-bg breadcrumb-bg-two">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-lg-6 col-md-8">
+                        <div className="breadcrumb-content text-center">
+                            <h3 className="title">fraction standard</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="fraction-section">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="fractional-tab">
+                            <a href="#" className="active">Choose Standard <i className="fas fa-chevron-right"></i></a>
+                            <a href="#">Select your NFTs</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-12 mt-5">
+                        <div className="row justify-content-center">
+                            <div className="col-lg-6 col-md-8">
+                                <div className="fractionalize-choose">
+                                    <div className="choose-detail">
+                                        <h2>Choose fraction standard</h2>
+                                        <p>Select a tokenization standard for the vault to determine how owners will be able to view and handle their fractions. Don’t worry, you’ll still be able to change your mind on the next page.</p>
+                                    </div>
+                                    <div className="wallet-list pt-3">
+
+                                        <Link href="/fractionstep2"><a className="wallet-box">
+                                            <div className="wallet-img">
+                                                <svg height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_2533:7409)"><path d="M2.33333 4.66667C2.541 4.66667 2.73933 4.63283 2.93067 4.58033L6.7585 8.40933L8.40817 6.75967L4.58033 2.93067C4.6305 2.73933 4.66667 2.541 4.66667 2.33333C4.66667 1.0465 3.6225 0 2.33333 0C1.04417 0 0 1.0465 0 2.33333C0 3.6225 1.04417 4.66667 2.33333 4.66667Z" fill="#AAED4A"></path> <path d="M2.33333 16.3327C3.19433 16.3327 3.93983 15.8613 4.3435 15.166H7V12.8327H4.3435C3.93867 12.1397 3.19433 11.666 2.33333 11.666C1.04417 11.666 0 12.7125 0 13.9993C0 15.2885 1.04417 16.3327 2.33333 16.3327Z" fill="#AAED4A"></path> <path d="M6.7585 19.5938L2.93067 23.4216C2.73933 23.3703 2.541 23.3341 2.33333 23.3341C1.04417 23.3341 0 24.3794 0 25.6674C0 26.9566 1.04417 28.0007 2.33333 28.0007C3.6225 28.0007 4.66667 26.9566 4.66667 25.6674C4.66667 25.4609 4.6305 25.2626 4.58033 25.0701L8.40817 21.2423L6.7585 19.5938Z" fill="#AAED4A"></path> <path d="M25.6667 11.666C24.8057 11.666 24.0613 12.1397 23.6565 12.8327H21V15.166H23.6565C24.0602 15.8613 24.8045 16.3327 25.6667 16.3327C26.9558 16.3327 28 15.2885 28 13.9993C28 12.7125 26.9558 11.666 25.6667 11.666Z" fill="#AAED4A"></path> <path d="M21.2415 8.40817L25.0693 4.57917C25.2606 4.63283 25.459 4.66667 25.6666 4.66667C26.9558 4.66667 28 3.6225 28 2.33333C28 1.0465 26.9558 0 25.6666 0C24.3775 0 23.3333 1.0465 23.3333 2.33333C23.3333 2.541 23.3695 2.73933 23.4196 2.93067L19.5918 6.75967L21.2415 8.40817Z" fill="#AAED4A"></path> <path d="M25.6666 23.3341C25.459 23.3341 25.2606 23.3703 25.0693 23.4216L21.2415 19.5938L19.5918 21.2434L23.4196 25.0712C23.3695 25.2626 23.3333 25.4609 23.3333 25.6674C23.3333 26.9566 24.3775 28.0007 25.6666 28.0007C26.9558 28.0007 28 26.9566 28 25.6674C28 24.3794 26.9558 23.3341 25.6666 23.3341Z" fill="#AAED4A"></path> <path d="M18.6663 14.0007C18.6663 11.4293 16.5726 9.33398 13.9997 9.33398C11.4268 9.33398 9.33301 11.4293 9.33301 14.0007C9.33301 16.5735 11.4268 18.6673 13.9997 18.6673C16.5726 18.6673 18.6663 16.5735 18.6663 14.0007Z" fill="#AAED4A"></path></g> <defs><clipPath id="clip0_2533:7409"><rect width="28" height="28" fill="white"></rect></clipPath></defs></svg>
+                                            </div>
+                                            <div className="wallet-detail">
+                                                <h3>Tokenize the vault ERC 20</h3>
+                                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sit soluta laborum incidunt suscipit quae, rerum, nam quo corporis ut, id, adipisci asperiores itaque dolor voluptates? Corporis </p>
+                                            </div>
+                                        </a></Link>
+                                        <Link href="/fractionstep2"><a className="wallet-box">
+                                            <div className="wallet-img">
+                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="4" cy="4" r="4" fill="#AAED4A"></circle> <circle cx="4" cy="20" r="4" fill="#AAED4A"></circle> <circle cx="20" cy="4" r="4" fill="#AAED4A"></circle> <circle cx="20" cy="20" r="4" fill="#AAED4A"></circle></svg>
+                                            </div>
+                                            <div className="wallet-detail">
+                                                <h3>Mint new NFTs ERC 721</h3>
+                                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sit soluta laborum incidunt suscipit quae, rerum, nam quo corporis ut, id, adipisci asperiores itaque dolor voluptates? Corporis </p>
+                                            </div>
+                                        </a></Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-3 col-md-4">
+                                <div className="fractionalize-right">
+                                    <h3>Need Help ?</h3>
+                                    <div className="fractionalize-right-box">
+                                        <picture><img src="assets/img/others/rightbanner.png" className="img-fluid" alt="" /></picture>
+                                        <label>How to guide</label>
+                                        <h4>Things to Know Before Fractionalizing NFT(s)</h4>
+                                    </div>
+                                    <a href="#" className="btn w-100">View All Guide</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </>);
+}
+
+export default FractionStep1Main;
