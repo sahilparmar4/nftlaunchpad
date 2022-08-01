407429d938fd335421b30e6b0486297f83e61d3e
Sahil Parmar <78207908+sahilparmar4@users.noreply.github.com>
2022-08-01 14:55:36 +0530

    Create marketplace_data.js

---
+++ website/components/marketplace/marketplace_data.js
@@ -0,0 +1,139 @@
+import Link from "next/link";
+
+const MarketplaceData = () => {
+
+    return (<>
+
+        <div className="row justify-content-center ">
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="top-collection-item">
+                    <div className="collection-item-thumb">
+                        <div className="shield-icon">
+                            <picture><img alt="" src="assets/img/others/shield.png" /></picture>
+                        </div>
+                        <Link href="/nftdetails">
+                            <picture><img src="assets/img/others/1top_collection01.jpg" alt="" /></picture></Link>
+                    </div>
+                    <div className="collection-item-content">
+                        <h5 className="title"><Link href="/nftdetails">NFT Collection</Link> <span className="price">5.4 ETH</span></h5>
+                    </div>
+                    <div className="collection-item-bottom">
+                        <ul>
+                            <li className="avatar"><Link href="/nftdetails"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></Link>By <a href="#" className="name">Jonson</a></li>
+                            <li className="wishlist"><a href="#">59</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="top-collection-item">
+                    <div className="collection-item-thumb">
+                        <div className="shield-icon">
+                            <picture><img alt="" src="assets/img/others/shield.png" /></picture>
+                        </div>
+                        <Link href="/nftdetails">
+                            <picture><img src="assets/img/others/2top_collection01.jpg" alt="" /></picture></Link>
+                    </div>
+                    <div className="collection-item-content">
+                        <h5 className="title"><Link href="/nftdetails">NFT Collection</Link> <span className="price">5.4 ETH</span></h5>
+                    </div>
+                    <div className="collection-item-bottom">
+                        <ul>
+                            <li className="avatar"><Link href="/nftdetails"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></Link>By <a href="#" className="name">Jonson</a></li>
+                            <li className="wishlist"><a href="#">59</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="top-collection-item">
+                    <div className="collection-item-thumb">
+                        <div className="shield-icon">
+                            <picture><img alt="" src="assets/img/others/shield.png" /></picture>
+                        </div>
+                        <a href="#"><picture><img src="assets/img/others/3top_collection01.jpg" alt="" /></picture></a>
+                    </div>
+                    <div className="collection-item-content">
+                        <h5 className="title"><a href="#">NFT Collection</a> <span className="price">5.4 ETH</span></h5>
+                    </div>
+                    <div className="collection-item-bottom">
+                        <ul>
+                            <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                            <li className="wishlist"><a href="#">59</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="top-collection-item">
+                    <div className="collection-item-thumb">
+                        <div className="shield-icon">
+                            <picture><img alt="" src="assets/img/others/shield.png" /></picture>
+                        </div>
+                        <Link href="/nftdetails">
+                            <picture><img src="assets/img/others/4top_collection01.jpg" alt="" /></picture></Link>
+                    </div>
+                    <div className="collection-item-content">
+                        <h5 className="title"><Link href="/nftdetails">NFT Collection</Link> <span className="price">5.4 ETH</span></h5>
+                    </div>
+                    <div className="collection-item-bottom">
+                        <ul>
+                            <li className="avatar"><Link href="/nftdetails"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></Link>By <a href="#" className="name">Jonson</a></li>
+                            <li className="wishlist"><a href="#">59</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="top-collection-item">
+                    <div className="collection-item-thumb">
+                        <div className="shield-icon">
+                            <picture><img alt="" src="assets/img/others/shield.png" /></picture>
+                        </div>
+                        <Link href="/nftdetails"><picture><img src="assets/img/others/1top_collection01.jpg" alt="" /></picture></Link>
+                    </div>
+                    <div className="collection-item-content">
+                        <h5 className="title"><Link href="/nftdetails">NFT Collection</Link> <span className="price">5.4 ETH</span></h5>
+                    </div>
+                    <div className="collection-item-bottom">
+                        <ul>
+                            <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                            <li className="wishlist"><a href="#">59</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div className="top-collection-item">
+                    <div className="collection-item-thumb">
+                        <div className="shield-icon">
+                            <picture><img alt="" src="assets/img/others/shield.png" /></picture>
+                        </div>
+                        <Link href="/nftdetails"><picture><img src="assets/img/others/2top_collection01.jpg" alt="" /></picture></Link>
+                    </div>
+                    <div className="collection-item-content">
+                        <h5 className="title"><Link href="/nftdetails">NFT Collection</Link> <span className="price">5.4 ETH</span></h5>
+                    </div>
+                    <div className="collection-item-bottom">
+                        <ul>
+                            <li className="avatar"><a href="#" className="thumb"><picture><img src="assets/img/others/top_col_avatar.png" alt="" /></picture></a>By <a href="#" className="name">Jonson</a></li>
+                            <li className="wishlist"><a href="#">59</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-lg-12">
+                <div className="prenext-pagination">
+                    <a href="#"><i className="fas fa-chevron-left"></i> Prev</a>
+                    <a href="#">Next <i className="fas fa-chevron-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </>);
+
+}
+
+
+
+export default MarketplaceData;
